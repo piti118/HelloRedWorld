d9ea037a0bc5fcfc131d9d8b3b34971097a9eeeb
Piti <piti118@gmail.com>
2016-12-12 03:39:36 +0700

    use libary fansi

--- src/main/scala/HelloRedWorld.scala
+++ src/main/scala/HelloRedWorld.scala
@@ -1,7 +1,11 @@
+import fansi._
+
 object HelloRedWorld{
   def main(args: Array[String]) = {
     val colored = Console.RED + "Hello RED World" + Console.RESET
     println(colored)
+    val colorblue: fansi.Str = fansi.Color.Blue("Hello World Ansi!")
+    println(colorblue)
     HelloRedWorldLib.print()
   }
 }
